694b46257ee5ff46bce543bea3e0ef0f7cebe732
Natnael <56716832+Natty4@users.noreply.github.com>
2022-03-30 10:07:44 +0300

    Update celery.py

--- online_supermarket/celery.py
+++ online_supermarket/celery.py
@@ -4,8 +4,8 @@
 import os
 from celery import Celery
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'benazom.settings')
-app = Celery('benazom')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_supermarket.settings')
+app = Celery('online_supermarket')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
